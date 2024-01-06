0013c53f6d4b8f685bb777d2c626d2ede77bb62b
StudentAlleg <studentalleg-pilot@yahoo.com>
2024-01-05 16:44:32 -0800

    #141 fix ClassSelectionMenu accessing null roles

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/ClassSelectionMenu.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/builds/menus/ClassSelectionMenu.java
@@ -25,6 +25,9 @@ public class ClassSelectionMenu extends AbstractGui implements Windowed {
         int[] slots = new int[] {9, 10, 11, 12, 13, 14, 15, 16, 17};
         final Iterator<Role> iterator = roleManager.getRoles().iterator();
         for (int slot : slots) {
+            if (!iterator.hasNext()) {
+                break;
+            }
             final Role role = iterator.next();
             setItem(slot, new ClassSelectionButton(buildManager, skillManager, role, this));
         }
